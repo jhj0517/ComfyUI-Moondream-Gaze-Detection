8457577f58578e8f168765b22e74d6f24f2a40d3
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-01-12 19:06:41 +0900

    Add basic nodes

--- nodes.py
+++ nodes.py
@@ -1,6 +1,6 @@
 #  Package Modules
 import os
-from typing import Union, BinaryIO, Dict, List, Tuple, Optional
+from typing import (Union, BinaryIO, Dict, List, Tuple, Optional)
 import time
 
 #  ComfyUI Modules
@@ -8,76 +8,58 @@ import folder_paths
 from comfy.utils import ProgressBar
 
 #  Your Modules
-from .modules.calculator import CalculatorModel
+from .modules.inferencer.moondream_inferencer import MoondreamInferencer
 
 
 #  Basic practice to get paths from ComfyUI
 custom_nodes_script_dir = os.path.dirname(os.path.abspath(__file__))
-custom_nodes_model_dir = os.path.join(folder_paths.models_dir, "my-custom-nodes")
-custom_nodes_output_dir = os.path.join(folder_paths.get_output_directory(), "my-custom-nodes")
+custom_nodes_model_dir = os.path.join(folder_paths.models_dir, "moondream")
+os.makedirs(custom_nodes_model_dir, exist_ok=True)
 
 
-#  These are example nodes that only contains basic functionalities with some comments.
-#  If you need detailed explanation, please refer to : https://docs.comfy.org/essentials/custom_node_walkthrough
-#  First Node:
-class MyModelLoader:
-    #  Define the input parameters of the node here.
+def get_category_name():
+    return "Moondream Gaze Detection"
+
+
+# total_steps = 5
+# comfy_pbar = ProgressBar(total_steps)
+# #  Then, update the progress.
+# for i in range(1, total_steps):
+#     time.sleep(1)
+#     comfy_pbar.update(
+#         i)  # Alternatively, you can use `comfy_pbar.update_absolute(value)` to update the progress with absolute value.
+
+
+class MoondreamModelLoader:
     @classmethod
     def INPUT_TYPES(s):
-        my_models = ["Model A", "Model B", "Model C"]
-
         return {
-            #  If the key is "required", the value must be filled.
             "required": {
-                #  `my_models` is the list, so it will be shown as a dropdown menu in the node. ( So that user can select one of them. )
-                #  You must provide the value in the tuple format. e.g. ("value",) or (3,) or ([1, 2],) etc.
-                "model": (my_models,),
-                "device": (['cuda', 'cpu', 'auto'],),
+                "device": (['cuda', 'cpu'],),
             },
-            #  If the key is "optional", the value is optional.
-            "optional": {
-                "compute_type": (['float32', 'float16'],),
-            }
         }
 
-    #  Define these constants inside the node.
-    #  `RETURN_TYPES` is important, as it limits the parameter types that can be passed to the next node, in `INPUT_TYPES()` above.
-    RETURN_TYPES = ("MY_MODEL",)
-    RETURN_NAMES = ("my_model",)
-    #  `FUNCTION` is the function name that will be called in the node.
+    RETURN_TYPES = ("MOONDREAM_MODEL",)
+    RETURN_NAMES = ("model",)
     FUNCTION = "load_model"
-    #  `CATEGORY` is the category name that will be used when user searches the node.
-    CATEGORY = "CustomNodesTemplate"
+    CATEGORY = get_category_name()
 
-    #  In the function, use same parameter names as you specified in `INPUT_TYPES()`
     def load_model(self,
-                   model: str,
                    device: str,
-                   compute_type: Optional[str] = None,
-                   ) -> Tuple[CalculatorModel]:
-        calculator_model = CalculatorModel()
-        calculator_model.load_model(model, device, compute_type)
+                   ) -> Tuple[MoondreamInferencer]:
 
-        #  You can use `comfy.utils.ProgressBar` to show the progress of the process.
-        #  First, initialize the total amount of the process.
-        total_steps = 5
-        comfy_pbar = ProgressBar(total_steps)
-        #  Then, update the progress.
-        for i in range(1, total_steps):
-            time.sleep(1)
-            comfy_pbar.update(i)  #  Alternatively, you can use `comfy_pbar.update_absolute(value)` to update the progress with absolute value.
+        model_inferencer = MoondreamInferencer(model_dir=custom_nodes_model_dir)
+        model_inferencer.load_model(device=device)
 
-        #  Return the model as a tuple.
-        return (calculator_model, )
+        return (model_inferencer, )
 
 
-#  Second Node
-class CalculatePlus:
+class GazeDetection:
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "model": ("MY_MODEL", ),
+                "model": ("MOONDREAM_MODEL", ),
             },
             #  Specify the parameters with type and default value.
             "optional": {
@@ -86,67 +68,15 @@ class CalculatePlus:
             }
         }
 
-    RETURN_TYPES = ("INT",)
-    RETURN_NAMES = ("plus_value",)
-    FUNCTION = "plus"
-    CATEGORY = "CustomNodesTemplate"
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "gaze_detection"
+    CATEGORY = get_category_name()
 
-    def plus(self,
-             model: CalculatorModel,
-             a: Optional[int],
-             b: Optional[int],
-             ) -> Tuple[int]:
-        result = model.plus(a, b)
-        return (result, )
+    def gaze_detection(self,
+                       model: MoondreamInferencer,
+                       image: Optional[int],
+                       ) -> Tuple[int]:
+        fig, status = model.process_image(image, use_ensemble=False)
+        return (fig, )
 
-
-
-#  Third Node
-class CalculateMinus:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "model": ("MY_MODEL", ),
-                "a": ("INT", ),
-            },
-            "optional": {
-                "b": ("INT", {"default": 10}),
-            }
-        }
-
-    RETURN_TYPES = ("INT",)
-    RETURN_NAMES = ("minus_value",)
-    FUNCTION = "minus"
-    CATEGORY = "CustomNodesTemplate"
-
-    def minus(self,
-             model: CalculatorModel,
-             a: Optional[int],
-             b: Optional[int],
-             ) -> Tuple[int]:
-        result = model.minus(a, b)
-        return (result, )
-
-
-
-#  Output Node
-class ExampleOutputNode:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "value": ("INT", ),
-            },
-        }
-
-    #  If the node is output node, set this to True.
-    OUTPUT_NODE = True
-    RETURN_TYPES = ("INT",)
-    RETURN_NAMES = ("int",)
-    FUNCTION = "result"
-    CATEGORY = "CustomNodesTemplate"
-
-    def result(self,
-               value: int,) -> Tuple[int]:
-        return (value, )
